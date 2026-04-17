99035385d10baac0ef48dc254d7cd5cc6aafc504
Ada-555 <ada@ada555.dev>
2026-04-17 04:47:35 +0100

    Update: set base path for 404-museum subdirectory

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://ada-555.github.io',
+  site: 'https://ada-555.github.io/404-museum',
+  base: '/404-museum',
   output: 'static',
 });
